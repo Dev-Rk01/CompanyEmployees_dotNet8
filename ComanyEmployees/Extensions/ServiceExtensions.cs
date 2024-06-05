e1085791668173834861728ab9068a3ed0de27ab
Devanshu Abhishek <devanshusingh55da@gmail.com>
2024-06-05 05:53:58 +0530

    Initial commit

---
+++ ComanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace ComanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+
+                //WithOrigins("https://example.com")
+                //WithMethods("POST", "GET")
+                //WithHeaders("accept", "contenttype")
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureLoggerServices(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+}
---
+++ ComanyEmployees/Program.cs
@@ -0,0 +1,40 @@
+using ComanyEmployees.Extensions;
+using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerServices();
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

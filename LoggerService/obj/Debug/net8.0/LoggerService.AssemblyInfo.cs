65995b22f6443ae199d8768d1b394602c5873240
Dev_Iotasol <98996644+Dev-Rk01@users.noreply.github.com>
2024-06-25 09:45:36 +0530

    company

--- LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b5bd7dce6ec3bfacf23f114c9d7f89f1573819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52782972cfaa60da78095ebf32918e48fbd094f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -10,11 +10,11 @@ namespace Repository
 {
     internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
-        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext){}
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges) => 
+        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
             FindAll(trackChanges).OrderBy(c => c.Name).ToList();
 
-        public Company GetCompany(Guid companyId, bool trackChanges) => 
+        public Company GetCompany(Guid companyId, bool trackChanges) =>
             FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
     }
 }
